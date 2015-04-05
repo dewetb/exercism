84a7e5e82b27e602d23ad5fa93a6d01d679f19f7
De Wet Blomerus <dewet@blomerus.org>
2015-04-05 15:15:36 +0200

    iterate rna

--- ruby/rna-transcription/complement.rb
+++ ruby/rna-transcription/complement.rb
@@ -1,23 +1,29 @@
 class Complement
-  VALID_DNA = "CGTA"
-  VALID_RNA = "CGUA"
+  RNA_COMPLEMENTS = {
+    'G' => 'C',
+    'C' => 'G',
+    'T' => 'A',
+    'A' => 'U'
+  }
+  DNA_COMPLEMENTS = RNA_COMPLEMENTS.invert
 
-  def self.validate(strand, complement)
-    strand.length.times do |i|
-      unless strand[i] != complement[i]
-        raise ArgumentError.new("not a valid strand")
-      end
+  def self.of_dna(rna)
+    compare_with(rna, RNA_COMPLEMENTS)
+  end
+
+  def self.of_rna(dna)
+    compare_with(dna, DNA_COMPLEMENTS)
+  end
+
+  private
+
+  def self.compare_with(rna_or_dna, complements)
+    results = []
+
+    rna_or_dna.split('').each do |cytosine|
+      results << complements[cytosine]
     end
-    complement
-  end
 
-  def self.of_dna(dna)
-    complement = dna.tr VALID_DNA, "GCAU"
-    validate(dna, complement)
-  end
-
-  def self.of_rna(rna)
-    complement = rna.tr VALID_RNA, "GCAT"
-    validate(rna, complement)
+    results.join()
   end
 end
