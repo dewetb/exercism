7b339b587c44b254c36fc40c4a6973a66b633950
De Wet Blomerus <dewet@blomerus.org>
2015-11-25 21:17:12 +0200

    brilliant or statement

--- ruby/leap/leap.rb
+++ ruby/leap/leap.rb
@@ -1,10 +1,6 @@
 class Year
   VERSION = 1
   def self.leap?(year)
-    if year % 100 == 0 && year % 400 == 0
-      true
-    elsif year % 4 == 0 && year % 100 != 0 
-      true
-    end
+    (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0)
   end
 end
