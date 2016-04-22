8a7fc41475a192dee06eef4bf3644931bcbdeee6
De Wet <dewet@blomerus.org>
2016-04-22 07:10:47 +0200

    count makes this so beautiful I could cry

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,9 +1,6 @@
 class Hamming
   def self.compute(a, b)
     zipped = a.chars.zip(b.chars)
-    zipped.inject(0) do |sum, set|
-      sum += 1 if set[0] != set[1]
-      sum
-    end
+    zipped.count { |set| set[0] != set[1] }
   end
 end
