f56e40347d9d82f841a66bf0c719ff33b3efa760
De Wet Blomerus <dewet@gitlab.com>
2017-12-01 12:21:41 +0200

    working but needs refactor

--- ecmascript/simple-cipher/simple-cipher.js
+++ ecmascript/simple-cipher/simple-cipher.js
@@ -1,6 +1,51 @@
 class Cipher {
   constructor() {
-    this.key = 'd'
+    this.key = 'bcbbbbbbbb'
+  }
+
+  numKey() {
+    let keyArr = [...this.key]
+    return keyArr.map(function(keyChar) {
+      return this.letterToNumber(keyChar);
+    }, this);
+  }
+
+  encode(plain) {
+    let plainArr = [...plain]
+    let encryptedArr = plainArr.map(function(plainChar, index) {
+      let plainNum = this.letterToNumber(plainChar);
+      let encryptedNum = plainNum + this.numKey()[index];
+      return this.numberToLetter(encryptedNum);
+    }, this);
+    return encryptedArr.join('');
+  }
+
+  decode(input) {
+    let inputArr = [...input]
+    let outputArr = inputArr.map(function(inputChar, index) {
+      let inputNum = this.letterToNumber(inputChar);
+      let outputNum = inputNum - this.numKey()[index];
+      return this.numberToLetter(outputNum);
+    }, this);
+    return outputArr.join('');
+  }
+
+  convert(input) {
+    let inputArr = [...input]
+    let outputArr = inputArr.map(function(inputChar, index) {
+      let inputNum = this.letterToNumber(inputChar);
+      let outputNum = inputNum - this.numKey()[index];
+      return this.numberToLetter(outputNum);
+    }, this);
+    return outputArr.join('');
+  }
+
+  letterToNumber(letter) {
+    return letter.charCodeAt(0) - 97
+  }
+
+  numberToLetter(number) {
+    return String.fromCharCode(97 + number);
   }
 }
 
