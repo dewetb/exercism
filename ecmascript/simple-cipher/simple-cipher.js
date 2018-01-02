bb8492851e4d6a2967e7f6cc90d4de16e9f38b2b
De Wet Blomerus <dewet@gitlab.com>
2018-01-02 22:34:25 +0200

    can encode

--- ecmascript/simple-cipher/simple-cipher.js
+++ ecmascript/simple-cipher/simple-cipher.js
@@ -1,6 +1,26 @@
 class Cipher {
   constructor() {
-    this.key = "something";
+    this.key = "bbbbbbbbbbbbbb";
+    this.keyArrNumbers = this.key.split('').map(letter => this.letterToNumber(letter));
+  }
+
+  encode(plain) {
+    let plainArr = plain.split('');
+    let encodedArr = plainArr.map(function(char, index) {
+      let inputNum = this.letterToNumber(char);
+      let diff = this.keyArrNumbers[index];
+      let outputLetter = this.numberToLetter(inputNum + diff)
+      return outputLetter;
+    }, this)
+    return encodedArr.join('');
+  }
+
+  letterToNumber(letter) {
+    return letter.charCodeAt(0) - 97;
+  }
+
+  numberToLetter(number) {
+    return String.fromCharCode(97 + number);
   }
 }
 
